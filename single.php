3df7258d20a0b2e1da3dd1b2bdde667ee8efa291
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-07-15 15:17:23 +0100

    Adding blog-post-item backend

--- single.php
+++ single.php
@@ -3,76 +3,42 @@
 <section class="blog-post">
   <div class="container">
     <div class="row">
-      <div class="col-lg-9 ps-sm-0 pe-sm-0 bg-white mb-3">
+      <div class="col-lg-9 ps-sm-0 pe-sm-0 ps-lg-3 pe-lg-3 bg-white mb-5">
         <div class="blog-post-item">
-          <div class="blog-post-img">
-            <img src="imgs/land.jpg" alt="blog-post-img" class="img-fluid rounded">
-          </div>
-          <div class="blog-post-author text-center">
-            <img src="imgs/avatar.png" alt="avatar" class="img-fluid rounded-circle">
-            <span class="fs-4">Abdelkarim</span>
-          </div>
-          <div class="blog-post-title d-flex justify-content-center">
-            <h1 class="text-center fs-1 w-75">Les nouvelles tendances en architecture durable</h1>
-          </div>
-          <div class="blog-post-data d-flex justify-content-evenly mb-5">
-            <span><i class="fa-solid fa-clock"></i>July 17, 2023</span>
-            <span><i class="fa-solid fa-comment"></i>20 Comments</span>
-            <span class="text-end"><i class="fa-solid fa-tag"></i>Nouveautés</span>
-          </div>
-          <div class="blog-post-content">
-            <p class="text-black-70 lh-lg p-sm-5">
-              L'architecture durable est devenue une priorité pour de nombreux concepteurs et architectes. Les
-              nouvelles
-              tendances
-              dans ce domaine mettent l'accent sur l'utilisation de matériaux écologiques, les techniques de
-              conception
-              bioclimatique
-              et les systèmes d'énergie renouvelable.
-
-              Une des tendances émergentes est l'utilisation de matériaux recyclés et écologiques dans la
-              construction.
-              Les
-              architectes recherchent des alternatives durables aux matériaux traditionnels tels que le bois, le béton
-              et le
-              métal.
-              Ils se tournent vers des matériaux comme le bambou, le liège, le bois récupéré et les briques fabriquées
-              à
-              partir de
-              déchets recyclés.
-
-              La conception bioclimatique est une autre tendance majeure en architecture durable. Il s'agit de
-              concevoir
-              des
-              bâtiments
-              qui exploitent les conditions climatiques locales pour réduire la consommation d'énergie. Cela peut
-              inclure
-              l'utilisation de la lumière naturelle, la ventilation naturelle, l'isolation thermique et l'orientation
-              des
-              bâtiments
-              pour maximiser l'efficacité énergétique.
-
-              Les systèmes d'énergie renouvelable gagnent également en popularité dans l'architecture durable. Les
-              panneaux
-              solaires,
-              les éoliennes et les systèmes de récupération de chaleur sont intégrés aux bâtiments pour réduire la
-              dépendance aux
-              sources d'énergie non renouvelables. Ces systèmes permettent de produire de l'électricité et de la
-              chaleur
-              de
-              manière
-              propre et durable.
-
-              En conclusion, les nouvelles tendances en architecture durable se concentrent sur l'utilisation de
-              matériaux
-              écologiques, la conception bioclimatique et l'intégration de systèmes d'énergie renouvelable. Ces
-              approches
-              permettent
-              de créer des bâtiments plus respectueux de l'environnement, économes en énergie et durables.
-            </p>
-          </div>
+          <?php
+            if (have_posts()) {
+              while (have_posts()) {
+                the_post();
+                ?>
+                  <div class="blog-post-img">
+                    <?php the_post_thumbnail('', ['class' => 'img-fluid rounded', 'alt' => 'Blog-post-img']); ?>
+                  </div>
+                  <div class="blog-post-author text-center">
+                    <img src="imgs/avatar.png" alt="avatar" class="img-fluid rounded-circle">
+                    <span class="fs-4"></i><?php the_author(); ?></span>
+                  </div>
+                  <div class="blog-post-title d-flex justify-content-center">
+                    <h1 class="text-center fs-1 w-75"><?php the_title(); ?></h1>
+                  </div>
+                  <div class="blog-post-data d-flex justify-content-evenly mb-5">
+                    <span><i class="fa-solid fa-clock"></i><?php the_time('F j, Y'); ?></span>
+                    <span><i class="fa-solid fa-comment"></i><?php comments_number(); ?></span>
+                    <span class="text-end"><i class="fa-solid fa-tag"></i><?php the_category(', '); ?></span>
+                  </div>
+                  <div class="blog-post-content">
+                    <p class="text-black-70 lh-lg p-sm-5">
+                      <?php the_content(); ?>
+                    </p>
+                  </div>
+                  <?php
+                }
+              }
+            ?>
         </div>
       </div>
+
+
+
       <div class="col-lg-3 ps-sm-0 pe-sm-0 ps-lg-3 pe-lg-3">
         <div class="card">
           <div class="card-header">
