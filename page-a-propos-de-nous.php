c0993f98a07cb08a2b61b6369d3c05a0082c39b4
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-07-20 23:00:29 +0100

    Adding About Page Structure

--- page-a-propos-de-nous.php
+++ page-a-propos-de-nous.php
@@ -1,3 +1,337 @@
 <?php get_header(); ?>
-
+<section class="about">
+  <div class="about-img">
+    <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/about.jpg" alt="About" class="img-fluid">
+  </div>
+  <p class="brief">
+    Chez <span>Stara Immo Travaux de construction</span>, nous sommes fiers d'être votre partenaire de confiance
+    dans tous vos projets
+    de construction. Depuis notre création, notre entreprise s'est engagée à offrir des services de construction de
+    haute
+    qualité, innovants et durables pour nos clients.
+  </p>
+  <section class="histoire">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <h1>Notre histoire</h1>
+          <p>
+            Fondée récemment par <span>M. Mbarek Achlaih</span>, <span>Stara Immo</span> est
+            une entreprise novatrice et passionnée
+            qui voit le jour grâce à la vision et à la détermination de son fondateur. Animé par une passion
+            profonde
+            pour
+            l'industrie de la construction et fort d'une expérience solide, <span>M. Mbarek Achlaih</span> a décidé
+            de
+            relever le défi d'établir
+            sa propre entreprise pour apporter une approche unique à l'univers de la construction.
+          </p>
+          <p>
+            Depuis ses modestes débuts, <span>Stara Immo</span> a connu une croissance
+            constante, et chaque
+            projet
+            réussi a renforcé sa réputation en tant qu'entreprise dévouée à l'excellence et à la satisfaction du
+            client. La
+            philosophie de <span>M. Mbarek Achlaih</span>, basée sur l'intégrité, la qualité et l'innovation,
+            imprègne
+            chaque aspect
+            de
+            l'entreprise et inspire notre équipe dédiée.
+          </p>
+          <p>
+            Notre société est le reflet de la vision et des valeurs de <span>M. Mbarek Achlaih</span>. Nous croyons
+            fermement que notre approche
+            centrée sur le client, notre engagement envers la qualité et notre recherche constante d'innovation nous
+            permettront de
+            nous démarquer dans l'industrie de la construction.
+          </p>
+          <p>
+            Chez <span>Stara Immo</span>, nous sommes fiers de notre héritage et de notre
+            héritage unique, et nous nous
+            efforçons de continuer à construire un avenir prospère pour notre entreprise et nos clients. Nous sommes
+            déterminés à
+            faire de chaque projet une réussite, en offrant des solutions de construction sur mesure et en
+            surpassant
+            les attentes
+            de nos clients.
+          </p>
+        </div>
+        <div class="col-lg-6">
+          <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/History.jpg" alt="History" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="histoire">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/boss.png" alt="boss" class="img-fluid">
+        </div>
+        <div class="col-lg-6">
+          <h1>Notre gérant</h1>
+          <p>
+            Le parcours de <span>M. Mbarek Achlaih</span> commence avec une longue carrière dans de grandes
+            entreprises de construction, où il a
+            acquis une expertise variée dans la réalisation de projets complexes. Fort de son expérience, il a
+            ressenti le besoin de
+            créer sa propre société, en mettant l'accent sur l'excellence, l'innovation et une approche centrée sur
+            le
+            client.
+          </p>
+          <p>
+            Avec une vision claire en tête, <span>M. Mbarek Achlaih</span> a travaillé sans relâche pour donner vie
+            à
+            <span>Stara Immo</span>. Grâce à son expertise, son dévouement et son réseau de
+            partenaires de confiance, il a réussi à établir
+            une entreprise solide, prête à relever les défis les plus audacieux de l'industrie.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="missions">
+    <div class="container">
+      <h1>Nos missions</h1>
+      <div class="row">
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/1.jpg" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Réalisation de projets résidentiels</h5>
+              <p class="card-text">Nous sommes spécialisés dans la construction de logements résidentiels, allant
+                des
+                maisons individuelles aux immeubles
+                d'appartements. Notre équipe d'experts travaille en étroite collaboration avec nos clients pour
+                donner
+                vie à leur
+                vision, en veillant à ce que chaque maison soit construite selon les normes les plus élevées de
+                qualité et de
+                durabilité.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/2.png" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Construction commerciale</h5>
+              <p class="card-text">Notre société de construction s'engage à créer des espaces commerciaux
+                fonctionnels, esthétiques et adaptés aux besoins
+                spécifiques de chaque entreprise. Nous collaborons étroitement avec nos clients pour comprendre
+                leurs
+                objectifs
+                commerciaux et réaliser des projets qui favorisent la productivité et l'efficacité de leur activité.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/3.jpg" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Projets industriels</h5>
+              <p class="card-text">Nous avons l'expertise nécessaire pour gérer des projets industriels complexes,
+                tels que des usines, des entrepôts et
+                des installations industrielles. Nous comprenons les exigences uniques de ce secteur et nous
+                efforçons
+                de livrer des
+                solutions de construction qui répondent aux normes de sécurité, de fonctionnalité et d'efficacité
+                les
+                plus élevées.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/4.jpg" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Rénovations et réhabilitations</h5>
+              <p class="card-text">En plus de la construction de nouveaux projets, nous proposons également des
+                services de rénovation et de
+                réhabilitation. Que ce soit pour des projets résidentiels, commerciaux ou industriels, notre équipe
+                est compétente pour
+                donner une nouvelle vie à des structures existantes, en les transformant en espaces modernes et
+                fonctionnels.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/5.jpg" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Gestion de projet</h5>
+              <p class="card-text">Nous prenons en charge la gestion complète de chaque projet, en veillant à ce que
+                toutes les étapes, de la conception à
+                la réalisation, soient gérées de manière efficace. Notre approche méthodique et notre gestion
+                rigoureuse des délais et
+                des budgets garantissent la réussite de chaque projet que nous entreprenons.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/6.png" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Durabilité et respect de l'environnement</h5>
+              <p class="card-text">Nous nous engageons à adopter des pratiques durables dans chaque projet de
+                construction que nous réalisons. Nous
+                privilégions l'utilisation de matériaux respectueux de l'environnement et des techniques de
+                construction
+                écoénergétiques, dans le but de minimiser notre impact sur l'environnement et de contribuer à la
+                construction d'un
+                avenir plus vert.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/7.jpg" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Qualité et sécurité</h5>
+              <p class="card-text">La qualité et la sécurité sont au cœur de tout ce que nous faisons. Nous nous
+                engageons à utiliser des matériaux de
+                première qualité et à respecter les normes de construction les plus strictes. De plus, nous mettons
+                en
+                œuvre des
+                protocoles de sécurité rigoureux pour garantir un environnement de travail sûr pour nos équipes et
+                nos
+                clients.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div class="card">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/8.jpg" class="card-img-top" alt="Missions">
+            <div class="card-body">
+              <h5 class="card-title">Service clientèle exceptionnel</h5>
+              <p class="card-text">Notre priorité absolue est la satisfaction de nos clients. Nous nous efforçons de
+                fournir un service clientèle
+                exceptionnel en étant à l'écoute de leurs besoins, en répondant rapidement à leurs questions et en
+                les tenant informés
+                tout au long du processus de construction.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="values">
+    <div class="container">
+      <h1>Nos valeurs</h1>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="value">
+            <div class="row">
+              <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/Value-1.png" alt="Values" class="img-fluid">
+              </div>
+              <div class="col-10">
+                <h5>Intégrité</h5>
+                <p>L'intégrité est la pierre angulaire de notre entreprise. Nous nous engageons à adopter des
+                  pratiques commerciales
+                  éthiques, honnêtes et transparentes dans toutes nos interactions avec nos clients, nos partenaires
+                  et nos employés. Nous
+                  mettons un point d'honneur à respecter nos engagements et à agir de manière responsable, en
+                  veillant à ce que notre
+                  réputation soit fondée sur la confiance et la fiabilité.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="value">
+            <div class="row">
+              <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/Value-2.png" alt="Values" class="img-fluid">
+              </div>
+              <div class="col-10">
+                <h5>Excellence</h5>
+                <p>L'excellence est notre objectif ultime dans tout ce que nous entreprenons. Nous recherchons
+                  constamment les plus hauts
+                  standards de qualité dans la réalisation de chaque projet de construction. De la conception à
+                  l'exécution, nous nous
+                  efforçons de surpasser les attentes de nos clients en offrant des résultats exceptionnels, sans
+                  compromettre sur la
+                  qualité et l'attention aux détails.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="value">
+            <div class="row">
+              <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/Value-3.png" alt="Values" class="img-fluid">
+              </div>
+              <div class="col-10">
+                <h5>Innovation</h5>
+                <p>Nous croyons en la puissance de l'innovation pour relever les défis de l'industrie de la
+                  construction. Nous sommes
+                  ouverts au changement et à l'adoption de nouvelles technologies et de pratiques de pointe. Notre
+                  esprit innovant nous
+                  pousse à trouver des solutions créatives, efficaces et durables pour répondre aux besoins uniques
+                  de chaque projet.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="value">
+            <div class="row">
+              <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/'?>imgs/Value-4.png" alt="Values" class="img-fluid">
+              </div>
+              <div class="col-10">
+                <h5>Engagement envers la sécurité</h5>
+                <p>La sécurité est notre priorité absolue, tant pour nos employés que pour nos clients et les
+                  parties prenantes de nos
+                  projets. Nous créons un environnement de travail sécurisé en mettant en œuvre des mesures de
+                  sécurité strictes et en
+                  veillant à ce que nos équipes soient formées aux meilleures pratiques pour prévenir les accidents
+                  et les incidents.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="engagement">
+    <div class="container">
+      <h1>Engagement envers la durabilité</h1>
+      <p>En tant que société de construction responsable, nous nous engageons à adopter des pratiques durables dans
+        chacun de nos
+        projets. Nous recherchons constamment des moyens d'améliorer notre empreinte environnementale et d'incorporer
+        des
+        matériaux et des techniques respectueux de l'environnement dans nos constructions.</p>
+    </div>
+  </section>
+  <section class="contact">
+    <div class="container">
+      <p>
+        Nous sommes impatients de discuter de votre projet de construction et de vous proposer des solutions sur
+        mesure.
+        Pour en
+        savoir plus sur nos services ou pour obtenir un devis personnalisé, n'hésitez pas à nous contacter via notre
+        <a href="mailto:abdelkarima46@gmail.com">mail</a> ou
+        par <a href="tel:+212708130262">téléphone</a>, vous pouvez aussi <a href="page-demander-un-devis.php">demander
+          un devis gratuit</a>.
+      </p>
+      <p>
+        Merci de nous avoir choisis pour vos projets de construction. Nous sommes déterminés à faire de votre vision
+        une
+        réalité
+        solide et durable.
+      </p>
+    </div>
+  </section>
+</section>
 <?php get_footer(); ?>
